ae15572edf31736d99daa79366ea6e2d59746e66
hugopatiñooo19 <133272436+gfgdgdfgfgsg@users.noreply.github.com>
2024-09-22 14:47:09 -0600

    Create book.java

---
+++ PRACTICA 3  /book.java
@@ -0,0 +1,33 @@
+public class Book {
+    private String title;
+    private String author;
+    private String content;
+
+    public Book(String title, String author, String content) {
+        this.title = title;
+        this.author = author;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // Method to represent the book as a string
+    @Override
+    public String toString() {
+        return "Title: " + title + "\nAuthor: " + author + "\nContent:\n" + content;
+    }
+}
