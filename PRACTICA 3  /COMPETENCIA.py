0a51c359077049adc426bd70950e4863692ba733
hugopatiñooo19 <133272436+gfgdgdfgfgsg@users.noreply.github.com>
2024-09-24 07:16:51 -0600

    Create COMPETENCIA.py

---
+++ PRACTICA 3  /COMPETENCIA.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Ejemplo de uso del algoritmo Merge Sort
+arr = [6, 5, 3, 1, 8, 7, 2, 4]
+print("Lista desordenada:", arr)
+merge_sort(arr)
+print("Lista ordenada:", arr)
